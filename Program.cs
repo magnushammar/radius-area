fef89e839ec611bee945f115b74fe2bd68c98464
Magnus Hammar <magnus@tenk.se>
2022-11-14 21:19:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Calculate the area of a circle.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radius_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.Write("Enter the radius of the circle: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine("The area of the circle is: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
